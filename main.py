42088c03bed0514cbe054cc5553b905499c16ec1
ArpedXD <arpedjeyescomen@gmail.com>
2026-07-19 11:30:07 +0800

    added local

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://arpedxd.github.io"],
+    allow_origins=["https://arpedxd.github.io", "http://127.0.0.1:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
